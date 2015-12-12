08aa9a0b8e532f27a1c785e5bb3cecd08c4307fe
sivaramya <sivaramya14@gmail.com>
2015-12-12 12:42:24 +0530

    Create help3.php

---
+++ help3.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<script type="text/javascript" src="chemis.js"></script>
+<LINK REL=StyleSheet HREF="style.css" TYPE="text/css" >
+</head>
+
+<body >
+
+<div id='enter'>
+
+
+<h2><u><b>Production of other components</b></u></h2>
+
+
+<b>RR<sup><b>Br<sub>2</b></sub></sup><b>-></b>RBr<sub>2</sub>RBr<sub>2</sub><br>
+RR<sup><b>HBr</b></sup><b>-></b>RH<sub>2</sub>RBr<sub>2</sub><br>
+RR<sup><sup>d</sup><b>H<sub>2</sub>SO<sub>4</b></sub></sup><b>-></b>RH<sub>2</sub>RO</b><br><br>
+Examples:<br>
+<b>CHCH<sup><b>Br<sub>2</b></sub></sup><b>-></b>CHBr<sub>2</sub>CHBr<sub>2</sub><br>
+CHCH<sup><b>HBr</b></sup><b>-></b>CH<sub>3</sub>CHBr<sub>2</sub><br>
+CHCH<sup><sup>d</sup><b>H<sub>2</sub>SO<sub>4</b></sub></sup><b>-></b>CH<sub>3</sub>CHO</b><br>
+</div>
+
+
+</body>
+</html>
