3f75ff50f2ddd367096b0acd1176384098e49204
sivaramya <sivaramya14@gmail.com>
2015-12-12 12:39:07 +0530

    Create help.php

---
+++ help.php
@@ -0,0 +1,48 @@
+
+<html>
+<head>
+<script type="text/javascript" src="chemis.js"></script>
+<LINK REL=StyleSheet HREF="style.css" TYPE="text/css" >
+</head>
+
+<body >
+
+<div id='enter'>
+
+<h2><u><b>Production of Itharo Carbon</b></u></h2>
+
+<b><u> Production of Alkane</u></b><br><b>
+
+RCl<sup><b>(Mgdust)</b></sup><b>-></b>RMgCl<sup><b>(H<sub>2</sub>O)</b></sup><b>-></b>RH<br>
+RO<sup><b>(Zn/Hg)/<sup>c</sup>HCl</b></sup><b>-></b>RH<sub>2</sub><br>
+R=R<sup><b>(Ni/H<sub>2)</b></sub></sup><b>-></b>RHRH<br>
+RR<sup><b>(Ni/H<sub>2)</b></sup><b>-></b>RH<sub>2</sub>RH<sub>2</sub><br><br>
+Examples:<br>
+CH<sub>3</sub>Cl<sup><b>(Mgdust)</b></sup><b>-></b>CH<sub>3</sub>MgCl<sup><b>(H<sub>2</sub>O)</b></sup><b>-></b>CH<sub>4</sub><br>
+CH<sub>2</sub>O<sup><b>(Zn/Hg)/<sup>c</sup>HCl</b></sup><b>-></b>CH<sub>4</sub><br>
+CH<sub>2</sub>=CH<sub>2</sub><sup><b><(Ni/H<sub>2)</b></sub></sup><b>-></b>CH<sub>3</sub>CH<sub>3</sub><br>
+CHCH<sup><b>(Ni/H<sub>2)</b></sup><b>-></b>CH<sub>3</sub>CH<sub>3</sub><br>
+</b><br>
+<b><u> Production of Alkene</u></b><br>
+
+<b>RH<sub>2</sub>RHOH<sup><b>(Al<sub>2</sub>O<sub>3 </sub>or <sup>c</sup>H2SO<sub>4)</b></sub></sup><b>-></b>RH=RH<br>
+RHROH<sup><b>(ROH\KOH or alcoholic KOH)</b></sup><b>-></b>R=R<br><br>
+Examples:<br>
+<b>CH<sub>3</sub>CH<sub>2</sub>OH<sup><b>(Al<sub>2</sub>O<sub>3 </sub>or <sup>c</sup>H2SO<sub>4)</b></sub></sup><b>-></b>CH<sub>2</sub>=CH<sub>2</sub></sub><br>
+CH<sub>3</sub>CH<sub>2</sub>OH<sup><b>(ROH\KOH or alcoholic KOH)</b></sup><b>-></b>CH<sub>2</sub>=CH<sub>2</sub><br>
+</b>
+
+<b> <u>Production of Alkyne</u></b><br>
+<b>RHBr-RHBr<sup><b>ROH\KOH</b></sup><b>-></b>RR<br><br>
+Examples:<br>
+<b>CH<sub>2</sub>Br-CH<sub>2</sub>Br<sup><b>ROH\KOH</b></sup><b>-></b>CHCH<br></td>
+</b>
+
+
+
+
+</div>
+
+
+</body>
+</html>
