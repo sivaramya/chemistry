d7d09a62d7018caf214eb1fd6ee16ffd541afc18
sivaramya <sivaramya14@gmail.com>
2015-12-12 12:36:00 +0530

    Create output.php

---
+++ output.php
@@ -0,0 +1,31 @@
+
+
+<html>
+<head>
+<script type="text/javascript" src="chemis.js"></script>
+<LINK REL=StyleSheet HREF="style.css" TYPE="text/css" >
+</head>
+
+	
+	
+<body>
+<form method="post" action='ans.php' target="content">
+	<div id = "labels">OUTPUT</div>
+	<div id="inputgroup" title="Click one of these formula">
+	<div id='input'><input type='submit' name ='s2' value='CH3CH3' style="height:50px; width:100%; font-size:14pt;"></div>
+	<div id='input'><input type='submit' name ='s2' value='C2H6' style="height:50px; width:100%; font-size:14pt;"></div>
+	<div id='input'><input type='submit' name ='s2' value='CH2OHCH2OH' style="height:50px; width:100%; font-size:14pt;"></div>
+	<div id='input'><input type='submit' name ='s2' value='CH2BrCH2Br' style="height:50px; width:100%; font-size:14pt;"></div>
+	<div id='input'><input type='submit' name ='s2' value='CH3CH0HCH3' style="height:50px; width:100%; font-size:14pt;"></div>
+	<div id='input'><input type='submit' name ='s2' value='CH3CHCHCH3' style="height:50px; width:100%; font-size:14pt;"></div>
+	<div id='input'><input type='submit' name ='s2' value='CH3CH2CH2Br' style="height:50px; width:100%; font-size:14pt;"></div>
+	<div id='input'><input type='submit' name ='s2' value='CH3CH3' style="height:50px; width:100%; font-size:14pt;"></div>
+	<div id='input'><input type='submit' name ='s2' value='CH3CCH' style="height:50px; width:100%; font-size:14pt;"></div>
+	<div id='input'><input type='submit' name ='s2' value='CHCH' style="height:50px; width:100%; font-size:14pt;"></div>
+	<div id='input'><input type='submit' name ='s2' value='CH3CHCH2' style="height:50px; width:100%; font-size:14pt;"></div>
+	
+</div>
+</form>
+</body>
+</html>
+
